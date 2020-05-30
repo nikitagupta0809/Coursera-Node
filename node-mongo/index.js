6fd78dabec9f90a2680a3b84c9baa9cb15fceb2d
ng0035 <nikitagupta0809@gmail.com>
2020-05-30 14:52:38 +0530

    mongooseose part 2 completed

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,29 +4,32 @@ const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
-MongoClient.connect(url, (err, client)=>{
-    assert.equal(err, null);
+
+MongoClient.connect(url). then((client)=>{
     console.log("connected to the server");
     const db = client.db(dbname);
   
-    dboper.insertDocument(db, { name: "Chole Bhature", description: "Spicy north Indian dish"}, 'dishes', (result)=>{
+    dboper.insertDocument(db, { name: "Chole Bhature", description: "Spicy north Indian dish"}, 'dishes')
+    .then((result)=>{
         console.log("Insert document:\n", result.ops);
-
-        dboper.findDocuments(db, 'dishes', (docs)=>{
-            console.log('Found documents:\n', docs);
-        
-            dboper.updateDocument(db, {name:'Chole Bhature'}, { description: "Updated Pav Bhaji"}, 'dishes', (result)=>{
-                console.log("updated document:\n", result.result);
-
-                dboper.findDocuments(db, 'dishes', (docs)=>{
-                    console.log('Found updated documents:\n', docs);
-
-                    db.dropCollection('dishes', (result)=>{
-                        console.log('Dropped Collection: ', result);
-                        client.close();
-                    });
-                });        
-            });
-        });
-    });
-});
+        return dboper.findDocuments(db, 'dishes')
+    })
+    .then((docs)=>{
+        console.log('Found documents:\n', docs);
+        return dboper.updateDocument(db, {name:'Chole Bhature'}, { description: "Updated Pav Bhaji"}, 'dishes')
+    })
+    .then((result)=>{
+        console.log("updated document:\n", result.result);
+        return dboper.findDocuments(db, 'dishes')
+    })
+    .then((docs)=>{
+        console.log('Found updated documents:\n', docs);
+        return db.dropCollection('dishes')
+    }) 
+    .then((result)=>{
+            console.log('Dropped Collection: ', result);
+            client.close();
+    })
+    .catch((err)=> console.log(err));
+})
+.catch((err)=> console.log(err));
---
+++ node-mongoose/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+    console.log('connected to server');
+    Dishes.create({
+        name: 'Pav Bhaji',
+        description: 'Best Bombay dish.'
+    })
+
+    .then((dish)=>{
+        console.log(dish);
+        return Dishes.find({});
+    })
+    .then((dishes)=>{
+        console.log(dishes);
+        return Dishes.remove({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+});
